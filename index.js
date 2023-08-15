9bcc429555e69c7117b9511faa8916cf2d8a2023
seu-usersaneme <kadutec27@gmail.com>
2023-08-15 10:40:36 -0300

    feature confete

--- index.js
+++ index.js
@@ -52,10 +52,77 @@ function check() {
     for (pos of positions) {
         if (pos.every((item) => items.includes(item))) {
             alert("O JOGADOR '" + playerLastMove + "' GANHOU!");
-            init();
-            return;
-        }
-    }
+            const canvas = document.createElement('canvas');
+            document.body.appendChild(canvas);
+
+            canvas.style.position = 'fixed';
+            canvas.style.top = '0';
+            canvas.style.left = '0';
+            canvas.style.pointerEvents = 'none';
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+
+            const ctx = canvas.getContext('2d');
+
+            function randomInRange(min, max) {
+                return Math.random() * (max - min) + min;
+            }
+
+            function createConfettiPiece() {
+                const colors = ['#f44336', '#3f51b5', '#ffeb3b', '#4caf50', '#2196f3', '#ff9800', '#9c27b0', '#009688'];
+                const color = colors[Math.floor(Math.random() * colors.length)];
+
+                return {
+                    x: randomInRange(0, canvas.width),
+                    y: randomInRange(-canvas.height * 0.5, -50),
+                    r: randomInRange(5, 15),
+                    d: randomInRange(1, 5),
+                    color: color,
+                };
+            }
+
+            const confettiPieces = [];
+
+            function drawConfettiPiece(confetti) {
+                ctx.beginPath();
+                ctx.arc(confetti.x, confetti.y, confetti.r, 0, 2 * Math.PI);
+                ctx.fillStyle = confetti.color;
+                ctx.fill();
+            }
+
+            function animateConfetti() {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                for (let i = 0; i < confettiPieces.length; i++) {
+                    const confetti = confettiPieces[i];
+
+                    confetti.y += confetti.d;
+
+                    if (confetti.y > canvas.height) {
+                        confettiPieces.splice(i, 1);
+                        i--;
+                    }
+
+                    drawConfettiPiece(confetti);
+                }
+
+                requestAnimationFrame(animateConfetti);
+            }
+
+            function startConfetti() {
+                for (let i = 0; i < 100; i++) {
+                    confettiPieces.push(createConfettiPiece());
+                }
+
+                animateConfetti();
+            }
+
+            // Acione o efeito de confetes após determinado jogador vencer uma partida
+            startConfetti();
+                        init();
+                        return;
+                    }
+                }
 
     if (selected.filter((item) => item).length === 9) {
         alert("DEU EMPATE!");
